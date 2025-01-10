28256892c25173b102379377ec13d59799092a65
IT24104129 <it24104129@my.sliit.lk>
2025-01-10 14:17:38 +0530

    Update IT24102279Lab5Q2.java

--- lab5/IT24102279Lab5Q2.java
+++ lab5/IT24102279Lab5Q2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class IT24102279Lab5Q2{
+public class IT24104129Lab5Q2{
 	public static void main(String[] args){
 	System.out.println("Enter The number of new customers :");
 	Scanner inputInstance = new Scanner(System.in);
